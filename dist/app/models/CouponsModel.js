7a70eb90a0b8492c71f2b924b68ca8f54357a911
NhatTan_Dev <tandevolopment.fe@gmail.com>
2025-02-18 11:17:09 +0700

    Model brand

---
+++ dist/app/models/CouponsModel.js
@@ -0,0 +1,49 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importStar(require("mongoose"));
+const CouponSchema = new mongoose_1.Schema({
+    code: { type: String, required: true, unique: true },
+    discountType: {
+        type: String,
+        enum: ["PERCENTAGE", "FIXED"],
+        default: "PERCENTAGE",
+    },
+    value: { type: Number, required: true },
+    minOrderValue: { type: Number, default: 0 },
+    maxDiscount: { type: Number, default: null },
+    expiryDate: { type: Date, required: true },
+    usedCount: { type: Number, default: 0 },
+    status: {
+        type: String,
+        enum: ["ACTIVE", "EXPIRED", "USED_UP", "DISABLED", "PENDING"],
+        default: "ACTIVE",
+    },
+}, {
+    timestamps: true,
+});
+const CouponModel = mongoose_1.default.model("Coupons", CouponSchema);
+exports.default = CouponModel;
+//# sourceMappingURL=CouponsModel.js.map
